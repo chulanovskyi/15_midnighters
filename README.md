260cef6b29b6dcf82e62d9461017610d702ac145
chulanovskyi <skyrocker@mail.ru>
2016-10-12 16:28:41 +0300

    task ready

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 15_midnighters
+
+###Prerequisites:
+
+Run in console `pip install -r requirements.txt` to install 3rd party modules.
+
+---
+
+This module display all code-addictive programmers from [devman.org](https://devman.org)
+Obviously midnight starts from 12:00pm.
+
+###How to use:
+
+You can edit in-script constants to get result as you want.
+
+| Constant | Description |
+| --- | --- |
+| `MIDNIGHT_END` | determines who should be considered for a 'code-addictive' |
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,36 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from pytz import timezone
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+MIDNIGHT_END = 5
+
+
+def load_attempts():
+    link = 'https://devman.org/api/challenges/solution_attempts/'
+    api_json = requests.get(link, params={'page':1}).json()
+    pages = api_json['number_of_pages']
+    records = list()
+    for page in range(pages):
+        api_page = requests.get(link, params={'page':page+1}).json()
+        records.extend(api_page['records'])
+    return records
+
+
+def is_midnighter(user):
+    if not user['timestamp']:
+        return False
+    normal_time = datetime.fromtimestamp(user['timestamp'])
+    user_timezone = timezone(user['timezone'])
+    local_timezone = user_timezone.localize(normal_time)
+    if local_timezone.hour > MIDNIGHT_END:
+        return False
+    return True
+
 
 if __name__ == '__main__':
-  pass
+    attempts = load_attempts()
+    midnighters = {user['username'] for user in attempts if is_midnighter(user)}
+    print('Code-addictive programmers are:')
+    for ind, user in enumerate(midnighters):
+        print(ind+1, user)
